ae9fcbf12ececeee9fea2b44ad60a30e1e397e0e
郑健鑫10 <1509352912@qq.com>
2025-03-31 12:11:05 +0800

    发布动态（设计中）

--- src/competition/publish/dongtai.vue
+++ src/competition/publish/dongtai.vue
@@ -1,16 +1,29 @@
 <template>
   <view class="box">
-    <u-navbar class="nav" autoBack leftText="发布动态" :fixed="false" bgColor="#f7f7f7"> </u-navbar>
+    <u-navbar class="nav" autoBack leftText="发布动态" :fixed="false" bgColor="#fff"> </u-navbar>
     <view>
       <scroll-view scroll-y :style="{ height: windowHeight - bottomBoxHeight - statusBarHeight - 44 + 'px' }">
         <view class="contentBox">
           <u-upload :fileList="fileList" @afterRead="afterRead" @delete="deletePic" name="6" multiple :maxCount="6"
-            width="100" height="100" :deleteIcon="'close'" :deleteIconColor="'#ff0000'" :deleteIconSize="20">
-            <!-- <image src="@/static/addImage.png" mode="widthFix" style="margin-bottom:10px ;width: 100px;height: 100px;">
-            </image> -->
+            width="86" height="86" :deleteIcon="'close'" :deleteIconColor="'#ff0000'" :deleteIconSize="20">
+            <view class="custom-upload">
+              <view class="plusIcon">
+                <u-icon name="plus" size="13px" color="#CCCCCC"></u-icon>
+              </view>
+              <text class="upload-text">图片/视频</text>
+            </view>
           </u-upload>
-          <view style="width: 200px;height: 30px;margin-bottom: 10px;">
-            <image src="@/static/tips.png" mode="scaleToFill" style="width: 200px;height: 30px;" />
+          <view class="tipsBox">
+            <image class="topIcon" src="@/static/top.png" mode="scaleToFill" />
+            <view class="box1">
+              <image class="tipsIcon"
+                src="https://testfeifanpaopao.jireplayer.com/download/upload/ffpp_xcx/images/tips.png"
+                mode="scaleToFill" />
+              <text class="tipsText">
+                说说你最近一次运动
+              </text>
+              <image class="reloadIcon" src="@/static/reload.png" mode="scaleToFill" />
+            </view>
           </view>
           <view class="textBox">
             <u-input v-model="title" type="text" placeholder="添加标题" :maxlength="20" border="none" @input="inputEvent">
@@ -18,42 +31,72 @@
                 <view style="display: flex;align-items: center;">
                   <image src="@/static/clear.png" mode="scaleToFill" style="width: 20px;height: 20px;"
                     @click="onClearTitle" />
-                  <text style="color: #c0c4cc;font-size: 20px;">{{ titleLength }}</text>
+                  <text style="font-family: PingFang SC, PingFang SC;
+                       font-weight: 400;
+                       font-size: 12px;
+                       color: rgba(29,35,38,0.3);
+                       line-height: 16px;
+                       text-align: left;
+                       font-style: normal;
+                       text-transform: none;">{{ titleLength }}</text>
                 </view>
               </template>
             </u-input>
-            <u-divider></u-divider>
-            <textarea style="width: 100%;height: 300px;overflow: scroll;" v-model="titleContent" type="text"
-              maxlength="300" disable-default-padding="true" placeholder="和大家聊聊你的热爱吧..." border="none"></textarea>
+            <u-divider color="#F0F0F0"></u-divider>
+            <textarea placeholder-style="color:rgba(29,35,38,0.3);font-size:14px;font-weight: 400;"
+              style="width: 100%;height: 278px;overflow: scroll;" v-model="titleContent" type="text" maxlength="300"
+              disable-default-padding="true" placeholder="和大家聊聊你的热爱吧..." border="none"></textarea>
           </view>
         </view>
         <view class="siteSelectionBox">
           <view class="box1">
             <view class="left">
-              <image src="@/static/location.png" mode="scaleToFill" style="width: 30px;height: 30px;" />
-              <text>添加位置</text>
+              <image src="https://testfeifanpaopao.jireplayer.com/download/upload/ffpp_xcx/images/location.png"
+                mode="scaleToFill" style="width: 16px;height: 16px;" />
+              <text style="padding-left: 3px;
+                font-family: PingFang SC, PingFang SC;
+                font-weight: 400;
+                font-size: 14px;
+                color: #1D2326;
+                line-height: 22px;
+                text-align: left;
+                font-style: normal;
+                text-transform: none;">添加位置</text>
             </view>
             <view class="right">
               <text>去选择</text>
-              <image src="@/static/left.png" mode="scaleToFill" style="width: 30px;height: 30px;" />
+              <u-icon name="arrow-right" size="6 10" color="#CCCCCC"></u-icon>
             </view>
           </view>
           <view class="box2">
+            <image class="top2Icon" src="@/static/top2.png" mode="scaleToFill" />
+            <view class="scrollxBox">
+              <scroll-view scroll-x style="width:100%;">
+                <view class="cardBox">
+                  <view class="card">红盾大厦</view>
+                  <view class="card">体育西路(地铁站)</view>
+                  <view class="card">CityNote希诺</view>
+                  <view class="card">CityNote希诺</view>
+                </view>
+              </scroll-view>
+            </view>
           </view>
         </view>
       </scroll-view>
     </view>
     <view class="bottomBox">
       <view class="layoutBox">
-        <view class="draftBox">
-          <u-icon name="file-text" color="#000" size="24"></u-icon>
-          <view>存草稿</view>
+        <view class="part">
+          <view class="draftBox">
+            <u-icon name="file-text" color="#000" size="24"></u-icon>
+            <view>存草稿</view>
+          </view>
+          <view class="previewBox">
+            <u-icon name="eye" color="#000" size="24"></u-icon>
+            <view>预览</view>
+          </view>
         </view>
-        <view class="previewBox">
-          <u-icon name="eye" color="#000" size="24"></u-icon>
-          <view>预览</view>
-        </view>
-        <u-button shape="circle" color="#000 ">发布动态</u-button>
+        <u-button class="publishButton" color="#15181A ">发布动态</u-button>
       </view>
     </view>
   </view>
@@ -127,14 +170,32 @@ export default {
 }
 
 ::v-deep .u-input__content__field-wrapper__field.data-v-fdbb9fe6 {
-  // height: 50px;
-  // line-height: 50px;
   font-size: 14px !important;
+}
+
+::v-deep .data-v-0e3867ac .u-input__content__field-wrapper__field.data-v-fdbb9fe6 {
+  font-family: PingFang SC, PingFang SC;
+  font-weight: 400;
+  font-size: 16px;
+  color: rgba(29, 35, 38, 0.3);
+  line-height: 24px;
+  text-align: left;
+  font-style: normal;
+  text-transform: none;
+}
+
+::v-deep .u-button--square.data-v-3bf2dba7 {
+  border-radius: 40px;
+}
+
+::v-deep .u-button.data-v-3bf2dba7 {
+  width: 220px;
+  height: 44px;
 }
 
 
 .box {
-  background-color: #f7f7f7;
+  background-color: #fff;
   width: 100vw;
   height: 100vh;
 
@@ -142,17 +203,98 @@ export default {
     width: calc(100% - 60px);
     margin: 0 16px;
     padding: 12px;
-    background-color: #f0f0f0;
+    background-color: #F3F4F5;
     border-radius: 10px;
+
+    .tipsBox {
+      .topIcon {
+        position: relative;
+        top: 4px;
+        left: 13px;
+        width: 20px;
+        height: 8px;
+      }
+
+      .box1 {
+        display: flex;
+        align-items: center;
+        margin-bottom: 12px;
+        width: 208px;
+        height: 24px;
+        background: #FFFFFF;
+        border-radius: 4px 4px 4px 4px;
+
+        .tipsText {
+          padding: 0 5px;
+          width: 130px;
+          height: 12px;
+          font-family: YouSheBiaoTiHei, YouSheBiaoTiHei;
+          font-weight: 400;
+          font-size: 14px;
+          color: #1D2326;
+          line-height: 12px;
+          // text-align: left;
+          font-style: normal;
+          // text-transform: none;
+        }
+
+        .tipsIcon {
+          margin-left: 4px;
+          width: 45px;
+          height: 16px;
+        }
+
+        .reloadIcon {
+          width: 16px;
+          height: 16px;
+        }
+      }
+
+    }
+
+    .custom-upload {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff;
+      width: 86px;
+      height: 86px;
+      border-radius: 4px;
+
+      .plusIcon {
+        width: 28px;
+        height: 28px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .upload-text {
+        width: 55px;
+        height: 16px;
+        font-family: PingFang SC, PingFang SC;
+        font-weight: 400;
+        font-size: 12px;
+        color: rgba(29, 35, 38, 0.6);
+        line-height: 16px;
+        text-align: left;
+        font-style: normal;
+        text-transform: none;
+      }
+    }
 
     .textBox {
       width: calc(100% - 24px);
       padding: 12px;
       background-color: #fff;
+
+
     }
   }
 
   .siteSelectionBox {
+    margin-top: 16px;
     width: 100%;
 
     .box1 {
@@ -174,42 +316,105 @@ export default {
       }
     }
 
-    .box2 {}
+    .box2 {
+      margin: 0 16px;
+      width: calc(100% - 36px);
+
+
+
+      .top2Icon {
+        position: relative;
+        top: 4px;
+        left: 18px;
+        width: 20px;
+        height: 8px;
+      }
+
+      .scrollxBox {
+        padding: 0 6px;
+        width: calc(100% - 12px);
+        display: flex;
+        border-radius: 4px;
+        background-color: #F3F4F5;
+
+        .cardBox {
+          width: calc(100% - 12px);
+          display: flex;
+          align-items: center;
+          padding: 6px 0px;
+
+          .card {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 32px;
+            padding: 5px 8px;
+            border-radius: 4px;
+            margin-right: 8px;
+            background-color: #fff;
+            white-space: nowrap;
+          }
+        }
+      }
+
+
+
+
+    }
 
   }
 
   .bottomBox {
-    padding-top: 10px;
     width: 100%;
     background-color: #fff;
     position: absolute;
     bottom: 0;
-    padding-bottom: 50px;
+    padding-bottom: 34px;
 
     .layoutBox {
-      width: calc(100% - 32px);
+      width: calc(100% - 48px);
       margin: 0 16px;
       display: flex;
       align-items: center;
+      padding: 12px 0px;
+      padding-left: 16px;
+      justify-content: space-between;
 
-      .draftBox {
-        width: calc(30% - 12px);
-        margin: 0 6px;
+      .part {
         display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        font-size: 12px;
+
+        .draftBox {
+          width: 48px;
+          height: 44px;
+          margin-right: 16px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          font-size: 12px;
+        }
+
+        .previewBox {
+          width: 48px;
+          height: 44px;
+          margin-right: 11px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          font-size: 12px;
+        }
       }
 
-      .previewBox {
-        width: calc(30% - 12px);
-        margin: 0 6px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        font-size: 12px;
+      .publishButton {
+        font-family: PingFang SC, PingFang SC;
+        font-weight: 600;
+        font-size: 16px;
+        color: #FFFFFF;
+        line-height: 24px;
+        text-align: left;
+        font-style: normal;
+        text-transform: none;
       }
     }
   }
